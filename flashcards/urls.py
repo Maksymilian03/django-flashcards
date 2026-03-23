a2e146ebf2fb632105b6c5b0fbb0d96bd00d6ffb
Maksymilian03 <maksiu2003xd@gmail.com>
2026-03-23 19:48:55 +0100

    Dodano rejestrację użytkownika i widok przeglądu fiszek

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import index, dashboard, start_review
+from django.contrib.auth import views as auth_views
+from .views import index, dashboard, start_review, register, add_flashcard
 
 urlpatterns = [
     path('', index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('register/', register, name='register'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('dashboard/', dashboard, name='dashboard'),
+    path('add_flashcard/', add_flashcard, name='add_flashcard'),
     path('review/', start_review, name='start_review'),
 ]
--- flashcards/views.py
+++ flashcards/views.py
@@ -1,13 +1,18 @@
-from django.utils import timezone
+from pyexpat.errors import messages
+from django.contrib.auth.decorators import login_required
 
-from django.shortcuts import render
+from django.utils import timezone
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User 
+from .forms import FlashcardForm
+
+from django.shortcuts import redirect, render
 from .models import Flashcard, UserProgress
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html', {
-        'flashcards': Flashcard.objects.all(),
-        'progress': UserProgress.objects.filter(user=request.user)
+        
     })
 
 def dashboard(request):
@@ -27,9 +32,33 @@ def dashboard(request):
     })
 
 def start_review(request):
-    print(f"Starting review - {timezone.now()}")
-    to_review = UserProgress.objects.filter(user=request.user, next_review__lte=timezone.now()).first()
+    
+    to_review = UserProgress.objects.filter(user=request.user, next_review__lte=timezone.now()).all()
 
     return render(request, 'to_review.html', {
         'to_review': to_review
     })
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # Create the user
+            form.save()
+            return redirect('login')
+    else:   
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def add_flashcard(request):
+    if request.method == 'POST':
+        form = FlashcardForm(request.POST)
+        if form.is_valid():
+            flashcard = form.save()
+            UserProgress.objects.create(user=request.user, flashcard=flashcard, next_review=timezone.now())
+            return redirect('dashboard')
+    else:
+        form = FlashcardForm()
+    return render(request, 'add_flashcard.html', {'form': form})
